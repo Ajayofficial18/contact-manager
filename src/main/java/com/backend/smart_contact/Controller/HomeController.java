8d8b867932fef41d97a98a048a43bf4627f97a01
Ajay <ajgour14@gmail.com>
2024-10-17 00:46:54 +0530

    designing home and signup page

--- src/main/java/com/backend/smart_contact/Controller/HomeController.java
+++ src/main/java/com/backend/smart_contact/Controller/HomeController.java
@@ -2,23 +2,47 @@ package com.backend.smart_contact.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.backend.smart_contact.Entities.User;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/home")
+    // homeHandler
+    @RequestMapping("/")
     public String homeHandler(Model model){
-        model.addAttribute("message", "this is ajay gour Learning thymeleaf!");
         model.addAttribute("Title", "HomePage-ContactManager");
         return "home";
     }
 
+    // aboutHandler
     @RequestMapping("/about")
     public String aboutHandler(Model model){
-        model.addAttribute("message", "this is ajay gour Learning thymeleaf!");
         model.addAttribute("Title", "AboutPage-ContactManager");
         return "about";
     }
+
+    // signUpHandler
+    @RequestMapping("/signup")
+    public String signUpHandler(Model model){
+        model.addAttribute("Title", "SignUpPage-ContactManager");
+        model.addAttribute("user", new User());
+        return "signup";
+    }
+
+    // do_registerHandler
+    @RequestMapping(value = "/do_register", method = RequestMethod.POST)
+    public String do_registerHandler(@ModelAttribute("user") User user, @RequestParam(value = "agreement", defaultValue = "false") boolean agreement, Model model){
+        System.out.println("Agreement "+agreement);
+        System.out.println("Model "+model);
+        System.out.println("User "+user);
+        return "signup";
+    }
+
+    
 
 }
