1aef43a013603b5ef0ae92515c2e41c938ff59dc
Ajay <ajgour14@gmail.com>
2024-10-16 18:48:21 +0530

    added controller and base html file(fragments)

--- src/main/java/com/backend/smart_contact/Controller/HomeController.java
+++ src/main/java/com/backend/smart_contact/Controller/HomeController.java
@@ -1,13 +1,24 @@
 package com.backend.smart_contact.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/home")
-    public String homeHandler(){
+    public String homeHandler(Model model){
+        model.addAttribute("message", "this is ajay gour Learning thymeleaf!");
+        model.addAttribute("Title", "HomePage-ContactManager");
         return "home";
     }
+
+    @RequestMapping("/about")
+    public String aboutHandler(Model model){
+        model.addAttribute("message", "this is ajay gour Learning thymeleaf!");
+        model.addAttribute("Title", "AboutPage-ContactManager");
+        return "about";
+    }
+
 }
